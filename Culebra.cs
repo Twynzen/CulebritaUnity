ba3b876462f0123360578016cdbc0052f9c67359
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2020-01-02 13:07:49 -0500

    Creando borde del escenario
    Este script controla los bordes del escenario creando un borde de bloques que son cubos

---
+++ Culebra.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Culebra : MonoBehaviour
+{
+   public GameObject Bloque;
+   public GameObject Escenario;
+   public int Ancho, Alto;
+
+   private void Awake(){
+
+       CrearMuros();
+
+   }
+
+   private void CrearMuros(){
+
+       for(int x=0; x<Ancho; x++){
+
+           for(int y=0; y<Alto; y++){
+
+
+            if(x == 0 || x == Ancho-1 || y == 0 || y== Alto-1){ 
+
+                Vector3 posicion = new Vector3(x, y);
+                Instantiate(Bloque, posicion, Quaternion.identity, Escenario.transform);
+           
+             }
+
+           }
+
+       }
+
+   }
+}
